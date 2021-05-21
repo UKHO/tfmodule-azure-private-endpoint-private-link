e0a62a96fbf0d0eb957deeb31ee5c48db8c71c0e
Shaun <ashaun@ukho.gov.uk>
2021-05-21 12:07:34 +0100

    intial setup for private endpoint

--- main.tf
+++ main.tf
@@ -1,5 +1,68 @@
-resource "azurerm_resource_group" "rg" {
-    name = "${var.name}-RG"  
-    location = "UK South"
+locals {
+  subscription_id     = "SUB_ID"
+  pe_name = "m-${var.pe_identity}-${var.pe_environment}-pe"
+  pe_rg_name = "m-${var.pe_identity}-rg" 
 }
 
+provider "azurerm" {
+  alias = "src"
+}
+
+data "azurerm_resource_group" "main" {
+ name = var.pe_vnet_rg
+} 
+
+data "azurerm_virtual_network" "main" {
+ name                = var.pe_vnet_name
+ resource_group_name = data.azurerm_resource_group.main.name
+}
+
+data "azurerm_subnet" "subnet" {
+ name                 = var.pe_subnet_name
+ virtual_network_name = data.azurerm_virtual_network.main.name
+ resource_group_name  = data.azurerm_resource_group.main.name 
+}
+
+resource "azurerm_resource_group" "rg" { 
+ provider = azurerm.src 
+ name = local.pe_rg_name
+ location = var.location  
+}
+
+resource "azurerm_private_dns_zone" "main" {
+  provider            = azurerm.src
+  name                = var.dns_zone
+  resource_group_name = azurerm_resource_group.rg.name  
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "main" {
+  provider              = azurerm.src
+  name                  = var.vnet_link
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.main.name
+  virtual_network_id    = data.azurerm_virtual_network.main.id
+}
+
+resource "azurerm_private_endpoint" "main" {
+  depends_on         = [azurerm_resource_group.rg]  
+  provider            = azurerm.src
+  name                = local.pe_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.subnet.id
+
+  private_service_connection {
+    name                           = var.network_type
+    private_connection_resource_id = var.private_connection
+    is_manual_connection           = false
+    subresource_names             = ["sites"]    
+  } 
+
+  private_dns_zone_group {
+    name                 = var.zone_group
+    private_dns_zone_ids = [azurerm_private_dns_zone.main.id]                      
+  }
+}
+
+#enforce_private_link_endpoint_network_policies = true This need to be turned off to deploy pe
+#enforce_private_link_service_network_policies  = true -> false
--- variables.tf
+++ variables.tf
@@ -1,5 +1,58 @@
-variable "name" {
-  type = string
-  default = "test"
+variable "dns_zone" {
+    description = "alias to create private dns zone - be aware this is dependant on the endpoint"
+    default = "privatelink.azurewebsites.net"
 }
+
+variable "vnet_link" {
+    description = "alias of the virtual network link"
+    default = ""  
+}
+
+variable "location" {
+    default = "uksouth"
+}
+
+variable "network_type" {
+  description = "type of connection"
+  default = "network"
+}
+
+variable "private_connection" {
+    description = "endpoint resource id"	 
+    default = "/subscriptions/SUBID/resourceGroups/RGNAME/providers/Microsoft.Web/sites/APP_SERVICE_NAME" 
+}
+
+variable "zone_group" {
+    description = "private dns zone group"
+    default = ""   
+}
+
+variable "pe_identity" {
+    description = "identity that will create all the private endpoint resources required"
+    default = ""
+}
+
+variable "pe_environment" {
+    description = "environment for private endpoint"
+    default = "dev | prd | qa"
+}
+
+variable "pe_vnet_rg" {
+    description = "this is the rg for the spoke vnet"
+    default = ""
+}
+
+variable "pe_vnet_name" {
+    description = "vnet name for the private endpoint"
+    default = ""
+}
+
+variable "pe_subnet_name" {
+    description = "subname that the private endpoint will associate"
+    default = ""
+}
+
+
+
+
 
