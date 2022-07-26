41314853d2b707653d39bf88a999911926da9bf4
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2022-07-26 14:18:23 +0100

    Use of required_provider

--- azure.tf
+++ azure.tf
@@ -1,7 +1,11 @@
-provider "azurerm" {
-  alias = "hub"
-}
-
-provider "azurerm" {
-  alias = "spoke"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      configuration_aliases = [
+        azurerm.hub
+        azurerm.spoke
+      ]
+    }
+  }
 }
